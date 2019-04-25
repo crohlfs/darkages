e53b50a99fe0366d1135e6b7db51643499691705
Chris Rohlfs <crohlfs@users.noreply.github.com>
2019-04-26 00:58:45 +1200

    Split body into bodyId and gender

--- packages/packets/src/server/displayUser.ts
+++ packages/packets/src/server/displayUser.ts
@@ -5,7 +5,8 @@ import {
   uint16be,
   uint32be,
   uint8,
-  boolean
+  boolean,
+  transform
 } from "@darkages/binary-blocks";
 import { direction, string8 } from "../shared";
 
@@ -13,36 +14,56 @@ import { direction, string8 } from "../shared";
  * Packet ID 0x33
  */
 const displayUser = compile(
-  record(
-    field("x", uint16be),
-    field("y", uint16be),
-    field("direction", direction),
-    field("id", uint32be),
-    field("helmet", uint16be),
-    field("body", uint8),
-    field("arms", uint16be),
-    field("boots", uint8),
-    field("armor", uint16be),
-    field("shield", uint8),
-    field("weapon", uint16be),
-    field("hairColor", uint8),
-    field("bootsColor", uint8),
-    field("firstAccessoryColor", uint8),
-    field("firstAccessory", uint16be),
-    field("secondAccessoryColor", uint8),
-    field("secondAccessory", uint16be),
-    field("thirdAccessoryColor", uint8),
-    field("thirdAccessory", uint16be),
-    field("lanternSize", uint8),
-    field("restPosition", uint8),
-    field("overcoat", uint16be),
-    field("overcoatColor", uint8),
-    field("skinColor", uint8),
-    field("invisible", boolean),
-    field("faceShape", uint8),
-    field("nameStyle", uint8),
-    field("name", string8),
-    field("groupName", string8),
+  transform(
+    record(
+      field("x", uint16be),
+      field("y", uint16be),
+      field("direction", direction),
+      field("id", uint32be),
+      field("helmet", uint16be),
+      field("bodyAndGender", uint8),
+      field("arms", uint16be),
+      field("boots", uint8),
+      field("armor", uint16be),
+      field("shield", uint8),
+      field("weapon", uint16be),
+      field("hairColor", uint8),
+      field("bootsColor", uint8),
+      field("firstAccessoryColor", uint8),
+      field("firstAccessory", uint16be),
+      field("secondAccessoryColor", uint8),
+      field("secondAccessory", uint16be),
+      field("thirdAccessoryColor", uint8),
+      field("thirdAccessory", uint16be),
+      field("lanternSize", uint8),
+      field("restPosition", uint8),
+      field("overcoat", uint16be),
+      field("overcoatColor", uint8),
+      field("skinColor", uint8),
+      field("invisible", boolean),
+      field("faceShape", uint8),
+      field("nameStyle", uint8),
+      field("name", string8),
+      field("groupName", string8)
+    ),
+    function(val) {
+      const { arms, bodyAndGender, ...output } = val;
+
+      const isFemale = bodyAndGender >= 32;
+
+      return {
+        ...output,
+        gender: isFemale ? ("w" as "w") : ("m" as "m"),
+        body: bodyAndGender - (isFemale ? 32 : 16)
+      };
+    },
+    function(val) {
+      return {
+        ...val,
+        arms: val.armor,
+        bodyAndGender: (val.gender === "m" ? 16 : 32) + val.body
+      };
+    }
   )
 );
 
