598a0b869b6ef60a685c5e675e5631eebea7f0e9
Chris Rohlfs <crohlfs@users.noreply.github.com>
2019-04-30 21:16:20 +1200

    Fix movePlayer packet

--- packages/packets/src/server/movePlayer.ts
+++ packages/packets/src/server/movePlayer.ts
@@ -4,18 +4,45 @@ import {
   field,
   uint8,
   uint16be,
-  writeOnly
+  writeOnly,
+  transform
 } from "@darkages/binary-blocks";
-import { direction } from "../shared";
 
 /**
  * Packet ID 0x0b
  */
 export default compile(
   record(
-    field("direction", direction),
-    field("oldX", uint16be),
-    field("oldY", uint16be),
+    field(
+      "direction",
+      transform(
+        uint8,
+        function(value) {
+          return value === 0
+            ? "up"
+            : value === 1
+            ? "right"
+            : value === 2
+            ? "down"
+            : value === 3
+            ? "left"
+            : "refresh";
+        },
+        function(dir) {
+          return dir === "up"
+            ? 0
+            : dir === "right"
+            ? 1
+            : dir === "down"
+            ? 2
+            : dir === "left"
+            ? 3
+            : 4;
+        }
+      )
+    ),
+    field("x", uint16be),
+    field("y", uint16be),
     writeOnly(uint16be, 0x0b),
     writeOnly(uint16be, 0x0b),
     writeOnly(uint16be, 0x0b),
